085acdb6e02612adabe40cfd2e507cfb71b7741e
= <=>
2022-05-01 18:54:28 -0500

    Paso 7.1:corrigiendo test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -5,13 +5,13 @@ describe("Unit Test for Spiderman class", () => {
 
       //colocamos el código tal como lo queremos usar
       //instanciamos un objeto spiderman con la siguiente información
-      const andrewGarfield = new Spiderverse("Spiderman Sony",31,"Andrew Garfield",2, "sony")
+      const andrewGarfield = new Spiderverse("Spiderman Sony",31,"Andrew Garfield",2, "Sony")
 
       //Validamos que este código funcione de la forma esperada
       expect(andrewGarfield.name).toBe("Spiderman Sony")
       expect(andrewGarfield.age).toBe(31)
       expect(andrewGarfield.actor).toBe("Andrew Garfield")
-      expect(andrewGarfield.numb_movies).toBe(31)
+      expect(andrewGarfield.numb_movies).toBe(2)
       expect(andrewGarfield.cinema_study).toBe("Sony")
     });
   })
