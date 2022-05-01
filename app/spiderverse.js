f37114eb00fd76e8285998d415707767283556c7
= <=>
2022-05-01 18:33:59 -0500

    Paso 6:Creando clase 'Spiderverse' del archivo 'spiderverse' en carpeta app

---
+++ app/spiderverse.js
@@ -0,0 +1,11 @@
+class Spiderverse{
+    constructor(name,age,actor,numb_movies,cinema_study){
+        this.name=name,
+        this.age=age,
+        this.actor=actor,
+        this.numb_movies=numb_movies,
+        this.cinema_study=cinema_study
+    }
+}
+
+module.exports= Spiderverse
