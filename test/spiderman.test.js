98636f592aa2e0fa4689ba697b1d8eb425b3d2dc
= <=>
2022-05-01 18:50:22 -0500

    Paso 7: Agregando TDD para probar la creacion de un objeto a partir de la clase Spiderman

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,17 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderverse = require("../app/spiderverse");
+
+describe("Unit Test for Spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+
+      //colocamos el código tal como lo queremos usar
+      //instanciamos un objeto spiderman con la siguiente información
+      const andrewGarfield = new Spiderverse("Spiderman Sony",31,"Andrew Garfield",2, "sony")
+
+      //Validamos que este código funcione de la forma esperada
+      expect(andrewGarfield.name).toBe("Spiderman Sony")
+      expect(andrewGarfield.age).toBe(31)
+      expect(andrewGarfield.actor).toBe("Andrew Garfield")
+      expect(andrewGarfield.numb_movies).toBe(31)
+      expect(andrewGarfield.cinema_study).toBe("Sony")
     });
   })
